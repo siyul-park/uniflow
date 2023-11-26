985ee25d06c84271e853a618b58637d50c6400cf
siyul-park <siyual.bak@gmail.com>
2023-11-26 04:24:04 -0500

    docs: add comment in symbol

--- pkg/symbol/symbol.go
+++ pkg/symbol/symbol.go
@@ -8,38 +8,44 @@ import (
 )
 
 type (
+	// Symbol represents an object that binds a Node and a Spec.
 	Symbol struct {
 		Node node.Node
 		Spec scheme.Spec
 	}
 )
 
-var _ node.Node = (*Symbol)(nil)
-
+// ID returns the unique identifier of the Symbol, based on its Node.
 func (s *Symbol) ID() ulid.ULID {
 	return s.Node.ID()
 }
 
+// Kind returns the kind of the Symbol, based on its Spec.
 func (s *Symbol) Kind() string {
 	return s.Spec.GetKind()
 }
 
+// Namespace returns the namespace of the Symbol, based on its Spec.
 func (s *Symbol) Namespace() string {
 	return s.Spec.GetNamespace()
 }
 
+// Name returns the name of the Symbol, based on its Spec.
 func (s *Symbol) Name() string {
 	return s.Spec.GetName()
 }
 
+// Links returns the links of the Symbol, based on its Spec.
 func (s *Symbol) Links() map[string][]scheme.PortLocation {
 	return s.Spec.GetLinks()
 }
 
+// Port returns the specified port of the Symbol, based on its Node.
 func (s *Symbol) Port(name string) (*port.Port, bool) {
 	return s.Node.Port(name)
 }
 
+// Close closes the Symbol, invoking the Close method of its Node.
 func (s *Symbol) Close() error {
 	return s.Node.Close()
 }
