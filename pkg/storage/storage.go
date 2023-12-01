c4abf36c7de12db23f9843e32598084795edae26
siyul-park <siyual.bak@gmail.com>
2023-11-30 20:27:59 -0500

    refactor: storage

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -27,16 +27,14 @@ type Storage struct {
 // CollectionNodes is the name of the nodes collection in the storage.
 const CollectionNodes = "nodes"
 
-var (
-	indexesNode = []database.IndexModel{
-		{
-			Name:    "namespace_name",
-			Keys:    []string{scheme.KeyNamespace, scheme.KeyName},
-			Unique:  true,
-			Partial: database.Where(scheme.KeyName).NE(primitive.NewString("")).And(database.Where(scheme.KeyName).IsNotNull()),
-		},
-	}
-)
+var indexesNode = []database.IndexModel{
+	{
+		Name:    "namespace_name",
+		Keys:    []string{scheme.KeyNamespace, scheme.KeyName},
+		Unique:  true,
+		Partial: database.Where(scheme.KeyName).NE(primitive.NewString("")).And(database.Where(scheme.KeyName).IsNotNull()),
+	},
+}
 
 // New creates a new Storage instance.
 func New(ctx context.Context, config Config) (*Storage, error) {
@@ -53,34 +51,39 @@ func New(ctx context.Context, config Config) (*Storage, error) {
 		nodes:  nodes,
 	}
 
-	if exists, err := s.nodes.Indexes().List(ctx); err != nil {
+	if err := s.ensureIndexes(ctx); err != nil {
 		return nil, err
-	} else {
-		for _, index := range indexesNode {
-			index = database.IndexModel{
-				Name:    index.Name,
-				Keys:    index.Keys,
-				Unique:  index.Unique,
-				Partial: index.Partial,
-			}
+	}
 
-			var ok bool
-			for _, i := range exists {
-				if i.Name == index.Name {
-					if reflect.DeepEqual(i, index) {
-						s.nodes.Indexes().Drop(ctx, i.Name)
-					}
+	return s, nil
+}
+
+// ensureIndexes creates required indexes for the storage collection.
+func (s *Storage) ensureIndexes(ctx context.Context) error {
+	existingIndexes, err := s.nodes.Indexes().List(ctx)
+	if err != nil {
+		return err
+	}
+
+	for _, index := range indexesNode {
+		var indexExists bool
+
+		for _, existingIndex := range existingIndexes {
+			if existingIndex.Name == index.Name {
+				indexExists = true
+				if !reflect.DeepEqual(existingIndex, index) {
+					s.nodes.Indexes().Drop(ctx, existingIndex.Name)
 					break
 				}
 			}
-			if ok {
-				continue
-			}
+		}
+
+		if !indexExists {
 			s.nodes.Indexes().Create(ctx, index)
 		}
 	}
 
-	return s, nil
+	return nil
 }
 
 // Watch returns a Stream to track changes based on the provided filter.
@@ -94,6 +97,7 @@ func (s *Storage) Watch(ctx context.Context, filter *Filter) (*Stream, error) {
 	if err != nil {
 		return nil, err
 	}
+
 	return NewStream(stream), nil
 }
 
@@ -107,6 +111,7 @@ func (s *Storage) InsertOne(ctx context.Context, spec scheme.Spec) (ulid.ULID, e
 	if err := unstructured.Marshal(spec); err != nil {
 		return ulid.ULID{}, err
 	}
+
 	if unstructured.GetNamespace() == "" {
 		unstructured.SetNamespace(scheme.DefaultNamespace)
 	}
@@ -119,14 +124,18 @@ func (s *Storage) InsertOne(ctx context.Context, spec scheme.Spec) (ulid.ULID, e
 	}
 
 	var id ulid.ULID
-	if pk, err := s.nodes.InsertOne(ctx, unstructured.Doc()); err != nil {
+
+	pk, err := s.nodes.InsertOne(ctx, unstructured.Doc())
+	if err != nil {
 		return ulid.ULID{}, err
-	} else if err := primitive.Unmarshal(pk, &id); err != nil {
+	}
+
+	if err := primitive.Unmarshal(pk, &id); err != nil {
 		_, _ = s.nodes.DeleteOne(ctx, database.Where(scheme.KeyID).EQ(pk))
 		return ulid.ULID{}, err
-	} else {
-		return id, nil
 	}
+
+	return id, nil
 }
 
 // InsertMany inserts multiple scheme.Spec instances and returns their IDs.
@@ -135,12 +144,14 @@ func (s *Storage) InsertMany(ctx context.Context, objs []scheme.Spec) ([]ulid.UL
 	defer s.mu.RUnlock()
 
 	var docs []*primitive.Map
+
 	for _, spec := range objs {
 		unstructured := scheme.NewUnstructured(nil)
 
 		if err := unstructured.Marshal(spec); err != nil {
 			return nil, err
 		}
+
 		if unstructured.GetNamespace() == "" {
 			unstructured.SetNamespace(scheme.DefaultNamespace)
 		}
@@ -155,15 +166,18 @@ func (s *Storage) InsertMany(ctx context.Context, objs []scheme.Spec) ([]ulid.UL
 		docs = append(docs, unstructured.Doc())
 	}
 
-	ids := make([]ulid.ULID, 0)
-	if pks, err := s.nodes.InsertMany(ctx, docs); err != nil {
+	pks, err := s.nodes.InsertMany(ctx, docs)
+	if err != nil {
 		return nil, err
-	} else if err := primitive.Unmarshal(primitive.NewSlice(pks...), &ids); err != nil {
+	}
+
+	var ids []ulid.ULID
+	if err := primitive.Unmarshal(primitive.NewSlice(pks...), &ids); err != nil {
 		_, _ = s.nodes.DeleteMany(ctx, database.Where(scheme.KeyID).IN(pks...))
 		return nil, err
-	} else {
-		return ids, nil
 	}
+
+	return ids, nil
 }
 
 // UpdateOne updates a single scheme.Spec and returns success or failure.
@@ -176,6 +190,7 @@ func (s *Storage) UpdateOne(ctx context.Context, spec scheme.Spec) (bool, error)
 	if err := unstructured.Marshal(spec); err != nil {
 		return false, err
 	}
+
 	if unstructured.GetNamespace() == "" {
 		unstructured.SetNamespace(scheme.DefaultNamespace)
 	}
@@ -197,12 +212,14 @@ func (s *Storage) UpdateMany(ctx context.Context, objs []scheme.Spec) (int, erro
 	defer s.mu.RUnlock()
 
 	var unstructureds []*scheme.Unstructured
+
 	for _, spec := range objs {
 		unstructured := scheme.NewUnstructured(nil)
 
 		if err := unstructured.Marshal(spec); err != nil {
 			return 0, err
 		}
+
 		if unstructured.GetNamespace() == "" {
 			unstructured.SetNamespace(scheme.DefaultNamespace)
 		}
@@ -223,7 +240,7 @@ func (s *Storage) UpdateMany(ctx context.Context, objs []scheme.Spec) (int, erro
 		if ok, err := s.nodes.UpdateOne(ctx, filter, unstructured.Doc()); err != nil {
 			return count, err
 		} else if ok {
-			count += 1
+			count++
 		}
 	}
 
@@ -266,20 +283,23 @@ func (s *Storage) FindOne(ctx context.Context, filter *Filter, options ...*datab
 		return nil, err
 	}
 
-	if doc, err := s.nodes.FindOne(ctx, f, options...); err != nil {
+	doc, err := s.nodes.FindOne(ctx, f, options...)
+	if err != nil {
 		return nil, err
-	} else if doc != nil {
-		unstructured := scheme.NewUnstructured(doc)
-		if spec, ok := s.scheme.New(unstructured.GetKind()); !ok {
-			return unstructured, nil
-		} else if err := unstructured.Unmarshal(spec); err != nil {
-			return nil, err
-		} else {
-			return spec, nil
-		}
 	}
 
-	return nil, nil
+	if doc == nil {
+		return nil, nil
+	}
+
+	unstructured := scheme.NewUnstructured(doc)
+	if spec, ok := s.scheme.New(unstructured.GetKind()); !ok {
+		return unstructured, nil
+	} else if err := unstructured.Unmarshal(spec); err != nil {
+		return nil, err
+	} else {
+		return spec, nil
+	}
 }
 
 // FindMany returns multiple scheme.Spec instances matched by the filter.
@@ -292,26 +312,28 @@ func (s *Storage) FindMany(ctx context.Context, filter *Filter, options ...*data
 		return nil, err
 	}
 
-	var specs []scheme.Spec
-	if docs, err := s.nodes.FindMany(ctx, f, options...); err != nil {
+	docs, err := s.nodes.FindMany(ctx, f, options...)
+	if err != nil {
 		return nil, err
-	} else {
-		for _, doc := range docs {
-			if doc == nil {
-				continue
-			}
-			unstructured := scheme.NewUnstructured(doc)
-			if spec, ok := s.scheme.New(unstructured.GetKind()); !ok {
-				specs = append(specs, unstructured)
-			} else if err := unstructured.Unmarshal(spec); err != nil {
-				return nil, err
-			} else {
-				specs = append(specs, spec)
-			}
+	}
+
+	var specs []scheme.Spec
+	for _, doc := range docs {
+		if doc == nil {
+			continue
 		}
 
-		return specs, nil
+		unstructured := scheme.NewUnstructured(doc)
+		if spec, ok := s.scheme.New(unstructured.GetKind()); !ok {
+			specs = append(specs, unstructured)
+		} else if err := unstructured.Unmarshal(spec); err != nil {
+			return nil, err
+		} else {
+			specs = append(specs, spec)
+		}
 	}
+
+	return specs, nil
 }
 
 func (s *Storage) validate(unstructured *scheme.Unstructured) error {
