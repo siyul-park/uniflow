79d7912d3fe0c08868ac3f1af0abf81f3d98c467
siyul-park <siyual.bak@gmail.com>
2023-12-15 04:53:47 -0500

    test: add uniflow test

---
+++ cmd/uniflow/uniflow/cmd_test.go
@@ -0,0 +1,39 @@
+package uniflow
+
+import (
+	"bytes"
+	"context"
+	"testing"
+	"testing/fstest"
+	"time"
+
+	"github.com/siyul-park/uniflow/pkg/database/memdb"
+	"github.com/siyul-park/uniflow/pkg/hook"
+	"github.com/siyul-park/uniflow/pkg/scheme"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestExecute(t *testing.T) {
+	s := scheme.New()
+	h := hook.New()
+	db := memdb.New("")
+	fsys := make(fstest.MapFS)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	output := new(bytes.Buffer)
+
+	cmd := NewCmd(Config{
+		Scheme:   s,
+		Hook:     h,
+		FS:       fsys,
+		Database: db,
+	})
+	cmd.SetOut(output)
+	cmd.SetErr(output)
+	cmd.SetContext(ctx)
+
+	err := cmd.Execute()
+	assert.NoError(t, err)
+}
