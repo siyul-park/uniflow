32b893cf8b2a77f27c14a912f5285ded79a29854
siyul-park <siyual.bak@gmail.com>
2023-12-21 20:57:15 -0500

    feat: add delete cli

---
+++ pkg/cmd/cli/delete.go
@@ -0,0 +1,86 @@
+package cli
+
+import (
+	"context"
+	"io/fs"
+
+	"github.com/oklog/ulid/v2"
+	"github.com/siyul-park/uniflow/pkg/cmd/scanner"
+	"github.com/siyul-park/uniflow/pkg/database"
+	"github.com/siyul-park/uniflow/pkg/scheme"
+	"github.com/siyul-park/uniflow/pkg/storage"
+	"github.com/spf13/cobra"
+)
+
+// DeleteConfig represents the configuration for the delete command.
+type DeleteConfig struct {
+	Scheme   *scheme.Scheme
+	Database database.Database
+	FS       fs.FS
+}
+
+// NewDeleteCommand creates a new cobra.Command for the delete command.
+func NewDeleteCommand(config DeleteConfig) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "delete",
+		Short: "Delete resources to runtime",
+		RunE:  runDeleteCommand(config),
+	}
+
+	cmd.PersistentFlags().StringP(flagNamespace, toShorthand(flagNamespace), "", "Set the resource's namespace. If not set, use the default namespace")
+	cmd.PersistentFlags().StringP(flagFile, toShorthand(flagFile), "", "Set the file path to be deleted")
+
+	return cmd
+}
+
+func runDeleteCommand(config DeleteConfig) func(cmd *cobra.Command, args []string) error {
+	return func(cmd *cobra.Command, _ []string) error {
+		ctx := cmd.Context()
+
+		ns, err := cmd.Flags().GetString(flagNamespace)
+		if err != nil {
+			return err
+		}
+		file, err := cmd.Flags().GetString(flagFile)
+		if err != nil {
+			return err
+		}
+
+		st, err := storage.New(ctx, storage.Config{
+			Scheme:   config.Scheme,
+			Database: config.Database,
+		})
+		if err != nil {
+			return err
+		}
+
+		specs, err := scanner.New().
+			Scheme(config.Scheme).
+			Storage(st).
+			Namespace(ns).
+			FS(config.FS).
+			Filename(file).
+			Scan(ctx)
+		if err != nil {
+			return err
+		}
+
+		if err := deleteSpecs(ctx, st, specs); err != nil {
+			return err
+		}
+
+		return nil
+	}
+}
+
+func deleteSpecs(ctx context.Context, st *storage.Storage, specs []scheme.Spec) error {
+	var filter *storage.Filter
+	for _, spec := range specs {
+		filter = filter.And(storage.Where[ulid.ULID](scheme.KeyID).EQ(spec.GetID()).And(storage.Where[string](scheme.KeyNamespace).EQ(spec.GetNamespace())))
+	}
+
+	if _, err := st.DeleteMany(ctx, filter); err != nil {
+		return err
+	}
+	return nil
+}
---
+++ pkg/cmd/cli/delete_test.go
@@ -0,0 +1,65 @@
+package cli
+
+import (
+	"context"
+	"encoding/json"
+	"testing"
+	"testing/fstest"
+
+	"github.com/go-faker/faker/v4"
+	"github.com/siyul-park/uniflow/pkg/database/memdb"
+	"github.com/siyul-park/uniflow/pkg/node"
+	"github.com/siyul-park/uniflow/pkg/scheme"
+	"github.com/siyul-park/uniflow/pkg/storage"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDeleteCommand_Execute(t *testing.T) {
+	s := scheme.New()
+	db := memdb.New("")
+	fsys := make(fstest.MapFS)
+
+	st, _ := storage.New(context.Background(), storage.Config{
+		Scheme:   s,
+		Database: db,
+	})
+
+	filepath := "resource.json"
+	kind := faker.Word()
+
+	spec := &scheme.SpecMeta{
+		Kind:      kind,
+		Namespace: scheme.DefaultNamespace,
+		Name:      faker.Word(),
+	}
+
+	codec := scheme.CodecFunc(func(spec scheme.Spec) (node.Node, error) {
+		return node.NewOneToOneNode(nil), nil
+	})
+
+	s.AddKnownType(kind, &scheme.SpecMeta{})
+	s.AddCodec(kind, codec)
+
+	data, _ := json.Marshal(spec)
+
+	fsys[filepath] = &fstest.MapFile{
+		Data: data,
+	}
+
+	_, _ = st.InsertOne(context.Background(), spec)
+
+	cmd := NewDeleteCommand(DeleteConfig{
+		Scheme:   s,
+		Database: db,
+		FS:       fsys,
+	})
+
+	cmd.SetArgs([]string{"--file", filepath})
+
+	err := cmd.Execute()
+	assert.NoError(t, err)
+
+	r, err := st.FindOne(context.Background(), storage.Where[string](scheme.KeyName).EQ(spec.GetName()))
+	assert.NoError(t, err)
+	assert.Nil(t, r)
+}
