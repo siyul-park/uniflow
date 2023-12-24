a2185565cd40cbaed6875d5262900fd1e2aa7db7
siyul-park <siyual.bak@gmail.com>
2023-12-23 22:30:13 -0500

    refactor: change method order in table

--- pkg/symbol/table.go
+++ pkg/symbol/table.go
@@ -106,6 +106,19 @@ func (t *Table) LookupByName(namespace, name string) (*Symbol, bool) {
 	return nil, false
 }
 
+// Keys returns all Symbol's ID.
+func (t *Table) Keys() []ulid.ULID {
+	t.mu.RLock()
+	defer t.mu.RUnlock()
+
+	var ids []ulid.ULID
+	for id := range t.symbols {
+		ids = append(ids, id)
+	}
+
+	return ids
+}
+
 // Clear free all associated Symbols.
 func (t *Table) Clear() error {
 	t.mu.Lock()
@@ -118,19 +131,6 @@ func (t *Table) Clear() error {
 	}
 
 	return nil
-}
-
-// Keys returns all Symbol's ID.
-func (t *Table) Keys() []ulid.ULID {
-	t.mu.RLock()
-	defer t.mu.RUnlock()
-
-	var ids []ulid.ULID
-	for id := range t.symbols {
-		ids = append(ids, id)
-	}
-
-	return ids
 }
 
 func (t *Table) insert(sym *Symbol) error {
