38d03f22d9fe4aca351167e9b6eb353320624a07
siyul-park <siyual.bak@gmail.com>
2024-01-02 03:59:45 -0500

    refactor: simple test

--- pkg/database/collection_test.go
+++ pkg/database/collection_test.go
@@ -10,36 +10,20 @@ import (
 func TestMergeUpdateOptions(t *testing.T) {
 	opt := MergeUpdateOptions([]*UpdateOptions{
 		nil,
-		lo.ToPtr(UpdateOptions{
-			Upsert: nil,
-		}),
-		lo.ToPtr(UpdateOptions{
-			Upsert: lo.ToPtr(true),
-		}),
+		lo.ToPtr(UpdateOptions{Upsert: nil}),
+		lo.ToPtr(UpdateOptions{Upsert: lo.ToPtr(true)}),
 	})
 
-	assert.Equal(t, lo.ToPtr(UpdateOptions{
-		Upsert: lo.ToPtr(true),
-	}), opt)
+	assert.Equal(t, lo.ToPtr(UpdateOptions{Upsert: lo.ToPtr(true)}), opt)
 }
 
 func TestMergeFindOptions(t *testing.T) {
 	opt := MergeFindOptions([]*FindOptions{
 		nil,
-		lo.ToPtr(FindOptions{
-			Limit: lo.ToPtr(1),
-		}),
-		lo.ToPtr(FindOptions{
-			Skip: lo.ToPtr(1),
-		}),
-		lo.ToPtr(FindOptions{
-			Sorts: []Sort{{Key: "", Order: OrderASC}},
-		}),
+		lo.ToPtr(FindOptions{Limit: lo.ToPtr(1)}),
+		lo.ToPtr(FindOptions{Skip: lo.ToPtr(1)}),
+		lo.ToPtr(FindOptions{Sorts: []Sort{{Key: "", Order: OrderASC}}}),
 	})
 
-	assert.Equal(t, lo.ToPtr(FindOptions{
-		Limit: lo.ToPtr(1),
-		Skip:  lo.ToPtr(1),
-		Sorts: []Sort{{Key: "", Order: OrderASC}},
-	}), opt)
+	assert.Equal(t, lo.ToPtr(FindOptions{Limit: lo.ToPtr(1), Skip: lo.ToPtr(1), Sorts: []Sort{{Key: "", Order: OrderASC}}}), opt)
 }
