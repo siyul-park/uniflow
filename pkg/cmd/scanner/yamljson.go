759f3d8a581904122b2ccfb6f5463d50335ff149
siyul-park <siyual.bak@gmail.com>
2024-01-11 06:03:38 -0500

    fix: check is json when unmarshal

--- pkg/cmd/scanner/yamljson.go
+++ pkg/cmd/scanner/yamljson.go
@@ -2,15 +2,14 @@ package scanner
 
 import (
 	"encoding/json"
-	"net/http"
 
 	"gopkg.in/yaml.v3"
 )
 
 // UnmarshalYAMLOrJSON unmarshals data based on its content type, supporting both JSON and YAML formats.
 func UnmarshalYAMLOrJSON(data []byte, v any) error {
-	if http.DetectContentType(data) == "application/json" {
-		return json.Unmarshal(data, v)
+	if err := json.Unmarshal(data, v); err != nil {
+		return err
 	}
 	return yaml.Unmarshal(data, v)
 }
