f4db5da042e29a86cab41bcd11b9e573ce5f013a
siyul-park <siyual.bak@gmail.com>
2024-01-11 09:03:22 -0500

    fix: use json when unmarshal bytes

--- pkg/cmd/scanner/yamljson.go
+++ pkg/cmd/scanner/yamljson.go
@@ -8,8 +8,8 @@ import (
 
 // UnmarshalYAMLOrJSON unmarshals data based on its content type, supporting both JSON and YAML formats.
 func UnmarshalYAMLOrJSON(data []byte, v any) error {
-	if err := json.Unmarshal(data, v); err != nil {
-		return err
+	if err := json.Unmarshal(data, v); err == nil {
+		return nil
 	}
 	return yaml.Unmarshal(data, v)
 }
