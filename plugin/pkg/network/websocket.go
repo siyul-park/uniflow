47190f917429c3d995386a229a4672c1f443c370
siyul-park <siyual.bak@gmail.com>
2024-02-02 05:41:09 -0500

    feat: add more configuration

--- plugin/pkg/network/websocket.go
+++ plugin/pkg/network/websocket.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"net/url"
 	"sync"
+	"time"
 
 	"github.com/gorilla/websocket"
 	"github.com/siyul-park/uniflow/pkg/node"
@@ -14,28 +15,70 @@ import (
 )
 
 type WebSocketNode struct {
-	ioPort  *port.Port
-	inPort  *port.Port
-	outPort *port.Port
-	errPort *port.Port
-	mu      sync.RWMutex
+	upgrader *websocket.Upgrader
+	ioPort   *port.Port
+	inPort   *port.Port
+	outPort  *port.Port
+	errPort  *port.Port
+	mu       sync.RWMutex
 }
 
 var _ node.Node = (*WebSocketNode)(nil)
 
-var upgrader = websocket.Upgrader{}
-
 func NewWebsocketNode() *WebSocketNode {
 	n := &WebSocketNode{
-		ioPort:  port.New(),
-		inPort:  port.New(),
-		outPort: port.New(),
-		errPort: port.New(),
+		upgrader: &websocket.Upgrader{},
+		ioPort:   port.New(),
+		inPort:   port.New(),
+		outPort:  port.New(),
+		errPort:  port.New(),
 	}
 
 	n.ioPort.AddInitHook(port.InitHookFunc(n.action))
 
 	return n
+}
+
+func (n *WebSocketNode) HandshakeTimeout() time.Duration {
+	n.mu.RLock()
+	defer n.mu.RUnlock()
+
+	return n.upgrader.HandshakeTimeout
+}
+
+func (n *WebSocketNode) SetHandshakeTimeout(timeout time.Duration) {
+	n.mu.Lock()
+	defer n.mu.Unlock()
+
+	n.upgrader.HandshakeTimeout = timeout
+}
+
+func (n *WebSocketNode) ReadBufferSize() int {
+	n.mu.RLock()
+	defer n.mu.RUnlock()
+
+	return n.upgrader.ReadBufferSize
+}
+
+func (n *WebSocketNode) SetReadBufferSize(size int) {
+	n.mu.Lock()
+	defer n.mu.Unlock()
+
+	n.upgrader.ReadBufferSize = size
+}
+
+func (n *WebSocketNode) SetWriteBufferSize(size int) {
+	n.mu.Lock()
+	defer n.mu.Unlock()
+
+	n.upgrader.WriteBufferSize = size
+}
+
+func (n *WebSocketNode) WriteBufferSize() int {
+	n.mu.RLock()
+	defer n.mu.RUnlock()
+
+	return n.upgrader.WriteBufferSize
 }
 
 func (n *WebSocketNode) Port(name string) (*port.Port, bool) {
@@ -104,7 +147,7 @@ func (n *WebSocketNode) action(proc *process.Process) {
 				Header: inPayload.Header,
 			}
 
-			_, err := upgrader.Upgrade(w, r, nil)
+			_, err := n.upgrader.Upgrade(w, r, nil)
 			if err != nil {
 				return err
 			}
--- plugin/pkg/network/websocket_test.go
+++ plugin/pkg/network/websocket_test.go
@@ -3,6 +3,7 @@ package network
 import (
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/gorilla/websocket"
 	"github.com/phayes/freeport"
@@ -14,6 +15,36 @@ func TestNewWebsocketNode(t *testing.T) {
 	n := NewWebsocketNode()
 	assert.NotNil(t, n)
 	assert.NoError(t, n.Close())
+}
+
+func TestWebsocket_HandshakeTimeout(t *testing.T) {
+	n := NewWebsocketNode()
+	defer n.Close()
+
+	v := time.Second
+
+	n.SetHandshakeTimeout(v)
+	assert.Equal(t, v, n.HandshakeTimeout())
+}
+
+func TestWebsocket_ReadBufferSize(t *testing.T) {
+	n := NewWebsocketNode()
+	defer n.Close()
+
+	v := 64
+
+	n.SetReadBufferSize(v)
+	assert.Equal(t, v, n.ReadBufferSize())
+}
+
+func TestWebsocket_WriteBufferSize(t *testing.T) {
+	n := NewWebsocketNode()
+	defer n.Close()
+
+	v := 64
+
+	n.SetWriteBufferSize(v)
+	assert.Equal(t, v, n.WriteBufferSize())
 }
 
 func TestWebsocketNode_Port(t *testing.T) {
@@ -38,22 +69,24 @@ func TestWebsocketNode_Port(t *testing.T) {
 }
 
 func TestWebsocketNode_SendAndReceive(t *testing.T) {
-	port, err := freeport.GetFreePort()
-	assert.NoError(t, err)
-
-	http := NewHTTPNode(fmt.Sprintf(":%d", port))
-	defer http.Close()
-
-	ws := NewWebsocketNode()
-	defer ws.Close()
-
-	io1, _ := http.Port(node.PortIO)
-	io2, _ := ws.Port(node.PortIO)
-
-	io1.Link(io2)
-
-	assert.NoError(t, http.Listen())
-
-	_, _, err = websocket.DefaultDialer.Dial(fmt.Sprintf("ws://localhost:%d", port), nil)
-	assert.NoError(t, err)
+	t.Run("Upgrade", func(t *testing.T) {
+		port, err := freeport.GetFreePort()
+		assert.NoError(t, err)
+	
+		http := NewHTTPNode(fmt.Sprintf(":%d", port))
+		defer http.Close()
+	
+		ws := NewWebsocketNode()
+		defer ws.Close()
+	
+		io1, _ := http.Port(node.PortIO)
+		io2, _ := ws.Port(node.PortIO)
+	
+		io1.Link(io2)
+	
+		assert.NoError(t, http.Listen())
+	
+		_, _, err = websocket.DefaultDialer.Dial(fmt.Sprintf("ws://localhost:%d", port), nil)
+		assert.NoError(t, err)
+	})
 }
