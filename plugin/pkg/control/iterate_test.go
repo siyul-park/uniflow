fd28d6af2f43051f16705a0a96893b69ccf55c3d
siyul-park <siyual.bak@gmail.com>
2024-03-02 20:11:24 -0500

    test: add more test case

--- plugin/pkg/control/iterate_test.go
+++ plugin/pkg/control/iterate_test.go
@@ -32,55 +32,102 @@ func TestIterateNode_Port(t *testing.T) {
 }
 
 func TestIterateNode_SendAndReceive(t *testing.T) {
-	n := NewIterateNode()
-	defer n.Close()
+	t.Run("In -> Out -> In", func(t *testing.T) {
+		n := NewIterateNode()
+		defer n.Close()
 
-	in := port.NewOut()
-	in.Link(n.In(node.PortIn))
+		in := port.NewOut()
+		in.Link(n.In(node.PortIn))
 
-	out0 := port.NewIn()
-	n.Out(node.PortWithIndex(node.PortOut, 0)).Link(out0)
+		out := port.NewIn()
+		n.Out(node.PortOut).Link(out)
 
-	out1 := port.NewIn()
-	n.Out(node.PortWithIndex(node.PortOut, 1)).Link(out1)
+		proc := process.New()
+		defer proc.Close()
 
-	proc := process.New()
-	defer proc.Close()
+		inWriter := in.Open(proc)
+		outReader := out.Open(proc)
 
-	inWriter := in.Open(proc)
-	outReader0 := out0.Open(proc)
-	outReader1 := out1.Open(proc)
+		inPayload := primitive.NewSlice()
+		for i := 0; i < 4; i++ {
+			inPayload = inPayload.Append(primitive.NewString(faker.UUIDHyphenated()))
+		}
+		inPck := packet.New(inPayload)
 
-	inPayload := primitive.NewSlice()
-	for i := 0; i < 4; i++ {
-		inPayload = inPayload.Append(primitive.NewString(faker.UUIDHyphenated()))
-	}
-	inPck := packet.New(inPayload)
+		inWriter.Write(inPck)
 
-	inWriter.Write(inPck)
+		ctx, cancel := context.WithTimeout(context.TODO(), time.Second)
+		defer cancel()
 
-	ctx, cancel := context.WithTimeout(context.TODO(), time.Second)
-	defer cancel()
+		for i := 0; i < inPayload.Len(); i++ {
+			select {
+			case outPck := <-outReader.Read():
+				assert.Equal(t, inPayload.Get(i), outPck.Payload())
+				outReader.Receive(outPck)
+			case <-ctx.Done():
+				assert.Fail(t, "timeout")
+			}
+		}
 
-	for i := 0; i < inPayload.Len(); i++ {
 		select {
-		case outPck := <-outReader0.Read():
-			outReader0.Receive(outPck)
+		case <-inWriter.Receive():
 		case <-ctx.Done():
 			assert.Fail(t, "timeout")
 		}
-	}
+	})
 
-	select {
-	case outPck := <-outReader1.Read():
-		outReader1.Receive(outPck)
-	case <-ctx.Done():
-		assert.Fail(t, "timeout")
-	}
+	t.Run("In -> Out0 -> Out1 -> In", func(t *testing.T) {
+		n := NewIterateNode()
+		defer n.Close()
 
-	select {
-	case <-inWriter.Receive():
-	case <-ctx.Done():
-		assert.Fail(t, "timeout")
-	}
+		in := port.NewOut()
+		in.Link(n.In(node.PortIn))
+
+		out0 := port.NewIn()
+		n.Out(node.PortWithIndex(node.PortOut, 0)).Link(out0)
+
+		out1 := port.NewIn()
+		n.Out(node.PortWithIndex(node.PortOut, 1)).Link(out1)
+
+		proc := process.New()
+		defer proc.Close()
+
+		inWriter := in.Open(proc)
+		outReader0 := out0.Open(proc)
+		outReader1 := out1.Open(proc)
+
+		inPayload := primitive.NewSlice()
+		for i := 0; i < 4; i++ {
+			inPayload = inPayload.Append(primitive.NewString(faker.UUIDHyphenated()))
+		}
+		inPck := packet.New(inPayload)
+
+		inWriter.Write(inPck)
+
+		ctx, cancel := context.WithTimeout(context.TODO(), time.Second)
+		defer cancel()
+
+		for i := 0; i < inPayload.Len(); i++ {
+			select {
+			case outPck := <-outReader0.Read():
+				assert.Equal(t, inPayload.Get(i), outPck.Payload())
+				outReader0.Receive(outPck)
+			case <-ctx.Done():
+				assert.Fail(t, "timeout")
+			}
+		}
+
+		select {
+		case outPck := <-outReader1.Read():
+			outReader1.Receive(outPck)
+		case <-ctx.Done():
+			assert.Fail(t, "timeout")
+		}
+
+		select {
+		case <-inWriter.Receive():
+		case <-ctx.Done():
+			assert.Fail(t, "timeout")
+		}
+	})
 }
