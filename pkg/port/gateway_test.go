2b6dac6c14fe376980988777925745202dbf0952
siyul-park <siyual.bak@gmail.com>
2024-05-22 05:21:39 -0400

    test: add more test case

--- pkg/port/gateway_test.go
+++ pkg/port/gateway_test.go
@@ -21,3 +21,58 @@ func TestGateway_Write(t *testing.T) {
 	count := g.Write(pck, r)
 	assert.Equal(t, 1, count)
 }
+
+func TestGateway_Forward(t *testing.T) {
+	t.Run("forward", func(t *testing.T) {
+		r := NewReader()
+		defer r.Close()
+
+		count := 0
+		g := NewGateway([]*Reader{r}, ForwardHookFunc(func(pcks []*packet.Packet) bool {
+			count++
+			return true
+		}))
+		defer g.Close()
+
+		pck := packet.New(nil)
+
+		g.Write(pck, r)
+		assert.Equal(t, 1, count)
+
+		g.Write(pck, r)
+		assert.Equal(t, 2, count)
+	})
+
+	t.Run("drop", func(t *testing.T) {
+		w := NewWriter()
+		defer w.Close()
+
+		r := NewReader()
+		defer r.Close()
+
+		w.Link(r)
+
+		count := 0
+		g := NewGateway([]*Reader{r}, ForwardHookFunc(func(pcks []*packet.Packet) bool {
+			count++
+			return false
+		}))
+		defer g.Close()
+
+		pck := packet.New(nil)
+
+		w.Write(pck)
+		w.Write(pck)
+
+		<-r.Read()
+		<-r.Read()
+
+		g.Write(pck, r)
+		assert.Equal(t, 1, count)
+		assert.Equal(t, packet.None, <-w.Receive())
+
+		g.Write(pck, r)
+		assert.Equal(t, 2, count)
+		assert.Equal(t, packet.None, <-w.Receive())
+	})
+}
