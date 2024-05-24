a86b2980b6a40b6fde9e8aefa4ad67782ab83026
siyul-park <siyual.bak@gmail.com>
2024-05-24 06:23:42 -0400

    fix: compile error

--- plugin/pkg/network/websocket.go
+++ plugin/pkg/network/websocket.go
@@ -1,219 +1,190 @@
 package network
 
-// import (
-// 	"sync"
+import (
+	"sync"
 
-// 	"github.com/gorilla/websocket"
-// 	"github.com/samber/lo"
-// 	"github.com/siyul-park/uniflow/pkg/node"
-// 	"github.com/siyul-park/uniflow/pkg/packet"
-// 	"github.com/siyul-park/uniflow/pkg/port"
-// 	"github.com/siyul-park/uniflow/pkg/primitive"
-// 	"github.com/siyul-park/uniflow/pkg/process"
-// )
+	"github.com/gorilla/websocket"
+	"github.com/samber/lo"
+	"github.com/siyul-park/uniflow/pkg/node"
+	"github.com/siyul-park/uniflow/pkg/packet"
+	"github.com/siyul-park/uniflow/pkg/port"
+	"github.com/siyul-park/uniflow/pkg/primitive"
+	"github.com/siyul-park/uniflow/pkg/process"
+)
 
-// // WebSocketNode represents a node for establishing WebSocket client connections.
-// type WebSocketNode struct {
-// 	action  func(*process.Process, *packet.Packet) (*websocket.Conn, error)
-// 	ioPort  *port.InPort
-// 	inPort  *port.InPort
-// 	outPort *port.OutPort
-// 	errPort *port.OutPort
-// 	mu      sync.RWMutex
-// }
+// WebSocketNode represents a node for establishing WebSocket client connections.
+type WebSocketNode struct {
+	action  func(*process.Process, *packet.Packet) (*websocket.Conn, error)
+	ioPort  *port.InPort
+	inPort  *port.InPort
+	outPort *port.OutPort
+	errPort *port.OutPort
+	mu      sync.RWMutex
+}
 
-// // WebSocketPayload represents the payload structure for WebSocket messages.
-// type WebSocketPayload struct {
-// 	Type int             `map:"type"`
-// 	Data primitive.Value `map:"data,omitempty"`
-// }
+// WebSocketPayload represents the payload structure for WebSocket messages.
+type WebSocketPayload struct {
+	Type int             `map:"type"`
+	Data primitive.Value `map:"data,omitempty"`
+}
 
-// var _ node.Node = (*WebSocketNode)(nil)
+var _ node.Node = (*WebSocketNode)(nil)
 
-// func newWebSocketNode(action func(*process.Process, *packet.Packet) (*websocket.Conn, error)) *WebSocketNode {
-// 	n := &WebSocketNode{
-// 		action:  action,
-// 		ioPort:  port.NewIn(),
-// 		inPort:  port.NewIn(),
-// 		outPort: port.NewOut(),
-// 		errPort: port.NewOut(),
-// 	}
+func newWebSocketNode(action func(*process.Process, *packet.Packet) (*websocket.Conn, error)) *WebSocketNode {
+	n := &WebSocketNode{
+		action:  action,
+		ioPort:  port.NewIn(),
+		inPort:  port.NewIn(),
+		outPort: port.NewOut(),
+		errPort: port.NewOut(),
+	}
 
-// 	n.ioPort.AddInitHook(port.InitHookFunc(n.connect))
-// 	n.errPort.AddInitHook(port.InitHookFunc(n.catch))
+	n.ioPort.AddInitHook(port.InitHookFunc(n.connect))
 
-// 	return n
-// }
+	return n
+}
 
-// // In returns the input port with the specified name.
-// func (n *WebSocketNode) In(name string) *port.InPort {
-// 	n.mu.RLock()
-// 	defer n.mu.RUnlock()
+// In returns the input port with the specified name.
+func (n *WebSocketNode) In(name string) *port.InPort {
+	n.mu.RLock()
+	defer n.mu.RUnlock()
 
-// 	switch name {
-// 	case node.PortIO:
-// 		return n.ioPort
-// 	case node.PortIn:
-// 		return n.inPort
-// 	default:
-// 	}
+	switch name {
+	case node.PortIO:
+		return n.ioPort
+	case node.PortIn:
+		return n.inPort
+	default:
+	}
 
-// 	return nil
-// }
+	return nil
+}
 
-// // Out returns the output port with the specified name.
-// func (n *WebSocketNode) Out(name string) *port.OutPort {
-// 	n.mu.RLock()
-// 	defer n.mu.RUnlock()
+// Out returns the output port with the specified name.
+func (n *WebSocketNode) Out(name string) *port.OutPort {
+	n.mu.RLock()
+	defer n.mu.RUnlock()
 
-// 	switch name {
-// 	case node.PortOut:
-// 		return n.outPort
-// 	case node.PortErr:
-// 		return n.errPort
-// 	default:
-// 	}
+	switch name {
+	case node.PortOut:
+		return n.outPort
+	case node.PortErr:
+		return n.errPort
+	default:
+	}
 
-// 	return nil
-// }
+	return nil
+}
 
-// // Close closes all ports of the WebSocketNode.
-// func (n *WebSocketNode) Close() error {
-// 	n.mu.Lock()
-// 	defer n.mu.Unlock()
+// Close closes all ports of the WebSocketNode.
+func (n *WebSocketNode) Close() error {
+	n.mu.Lock()
+	defer n.mu.Unlock()
 
-// 	n.ioPort.Close()
-// 	n.inPort.Close()
-// 	n.outPort.Close()
-// 	n.errPort.Close()
+	n.ioPort.Close()
+	n.inPort.Close()
+	n.outPort.Close()
+	n.errPort.Close()
 
-// 	return nil
-// }
+	return nil
+}
 
-// func (n *WebSocketNode) connect(proc *process.Process) {
-// 	n.mu.RLock()
-// 	defer n.mu.RUnlock()
+func (n *WebSocketNode) connect(proc *process.Process) {
+	n.mu.RLock()
+	defer n.mu.RUnlock()
 
-// 	ioReader := n.ioPort.Open(proc)
+	ioReader := n.ioPort.Open(proc)
+	errWriter := n.errPort.Open(proc)
 
-// 	for {
-// 		inPck, ok := <-ioReader.Read()
-// 		if !ok {
-// 			return
-// 		}
+	for {
+		inPck, ok := <-ioReader.Read()
+		if !ok {
+			return
+		}
 
-// 		if conn, err := n.action(proc, inPck); err != nil {
-// 			n.throw(proc, err, inPck)
-// 		} else {
-// 			proc.Ref(1)
-// 			proc.Stack().Clear(inPck)
+		if conn, err := n.action(proc, inPck); err != nil {
+			errPck := packet.WithError(err, inPck)
+			backPck := packet.None
+			if errWriter.Write(errPck) > 0 {
+				backPck = <-errWriter.Receive()
+			}
+			ioReader.Receive(backPck)
+		} else {
+			child := proc.Fork()
 
-// 			go n.write(proc, conn)
-// 			go n.read(proc, conn)
-// 		}
-// 	}
-// }
+			go n.write(child, conn)
+			go n.read(child, conn)
 
-// func (n *WebSocketNode) write(proc *process.Process, conn *websocket.Conn) {
-// 	n.mu.RLock()
-// 	defer n.mu.RUnlock()
+			ioReader.Receive(packet.None)
+		}
+	}
+}
 
-// 	inReader := n.inPort.Open(proc)
+func (n *WebSocketNode) write(proc *process.Process, conn *websocket.Conn) {
+	n.mu.RLock()
+	defer n.mu.RUnlock()
 
-// 	for {
-// 		inPck, ok := <-inReader.Read()
-// 		if !ok {
-// 			_ = conn.Close()
-// 			return
-// 		}
+	inReader := n.inPort.Open(proc)
+	errWriter := n.errPort.Open(proc)
 
-// 		var inPayload *WebSocketPayload
-// 		if err := primitive.Unmarshal(inPck.Payload(), &inPayload); err != nil {
-// 			inPayload.Data = inPck.Payload()
-// 			if _, ok := inPayload.Data.(primitive.Binary); !ok {
-// 				inPayload.Type = websocket.TextMessage
-// 			} else {
-// 				inPayload.Type = websocket.BinaryMessage
-// 			}
-// 		}
+	for {
+		inPck, ok := <-inReader.Read()
+		if !ok {
+			_ = conn.Close()
+			return
+		}
 
-// 		if data, err := MarshalMIME(inPayload.Data, lo.ToPtr("")); err != nil {
-// 			n.throw(proc, err, inPck)
-// 		} else if err := conn.WriteMessage(inPayload.Type, data); err != nil {
-// 			n.throw(proc, err, inPck)
-// 		} else {
-// 			proc.Stack().Clear(inPck)
-// 		}
-// 	}
-// }
+		var inPayload *WebSocketPayload
+		if err := primitive.Unmarshal(inPck.Payload(), &inPayload); err != nil {
+			inPayload.Data = inPck.Payload()
+			if _, ok := inPayload.Data.(primitive.Binary); !ok {
+				inPayload.Type = websocket.TextMessage
+			} else {
+				inPayload.Type = websocket.BinaryMessage
+			}
+		}
 
-// func (n *WebSocketNode) read(proc *process.Process, conn *websocket.Conn) {
-// 	n.mu.RLock()
-// 	defer n.mu.RUnlock()
+		if data, err := MarshalMIME(inPayload.Data, lo.ToPtr("")); err != nil {
+			errPck := packet.WithError(err, inPck)
+			if errWriter.Write(errPck) > 0 {
+				<-errWriter.Receive()
+			}
+		} else if err := conn.WriteMessage(inPayload.Type, data); err != nil {
+			errPck := packet.WithError(err, inPck)
+			if errWriter.Write(errPck) > 0 {
+				<-errWriter.Receive()
+			}
+		}
 
-// 	outWriter := n.outPort.Open(proc)
-// 	port.Discard(outWriter)
+		inReader.Receive(packet.None)
+	}
+}
 
-// 	for {
-// 		typ, p, err := conn.ReadMessage()
-// 		if err != nil {
-// 			proc.Ref(-1)
-// 			return
-// 		}
+func (n *WebSocketNode) read(proc *process.Process, conn *websocket.Conn) {
+	n.mu.RLock()
+	defer n.mu.RUnlock()
 
-// 		data, err := UnmarshalMIME(p, lo.ToPtr(""))
-// 		if err != nil {
-// 			data = primitive.NewString(err.Error())
-// 		}
+	outWriter := n.outPort.Open(proc)
+	port.Discard(outWriter)
 
-// 		outPayload, _ := primitive.MarshalText(&WebSocketPayload{
-// 			Type: typ,
-// 			Data: data,
-// 		})
+	for {
+		typ, p, err := conn.ReadMessage()
+		if err != nil {
+			proc.Exit(nil)
+			return
+		}
 
-// 		outPck := packet.New(outPayload)
-// 		outWriter.Write(outPck)
-// 	}
-// }
+		data, err := UnmarshalMIME(p, lo.ToPtr(""))
+		if err != nil {
+			data = primitive.NewString(err.Error())
+		}
 
-// func (n *WebSocketNode) catch(proc *process.Process) {
-// 	n.mu.RLock()
-// 	defer n.mu.RUnlock()
+		outPayload, _ := primitive.MarshalText(&WebSocketPayload{
+			Type: typ,
+			Data: data,
+		})
 
-// 	ioReader := n.ioPort.Open(proc)
-// 	inReader := n.inPort.Open(proc)
-// 	errWriter := n.errPort.Open(proc)
-
-// 	for {
-// 		backPck, ok := <-errWriter.Receive()
-// 		if !ok {
-// 			return
-// 		}
-
-// 		ioCost := ioReader.Cost(backPck)
-// 		inCost := inReader.Cost(backPck)
-
-// 		if ioCost < inCost {
-// 			ok = ioReader.Receive(backPck)
-// 		} else {
-// 			ok = inReader.Receive(backPck)
-// 		}
-// 		if !ok {
-// 			proc.Stack().Clear(backPck)
-// 		}
-// 	}
-// }
-
-// func (n *WebSocketNode) throw(proc *process.Process, err error, cause *packet.Packet) {
-// 	errWriter := n.errPort.Open(proc)
-// 	ioReader := n.ioPort.Open(proc)
-
-// 	errPck := packet.WithError(err, cause)
-// 	proc.Stack().Add(cause, errPck)
-
-// 	if !errWriter.Write(errPck) {
-// 		if !ioReader.Receive(errPck) {
-// 			proc.Stack().Clear(errPck)
-// 		}
-// 	}
-// }
+		outPck := packet.New(outPayload)
+		outWriter.Write(outPck)
+	}
+}
--- plugin/pkg/network/websocketclient.go
+++ plugin/pkg/network/websocketclient.go
@@ -1,153 +1,155 @@
 package network
 
-// import (
-// 	"net/http"
-// 	"net/url"
-// 	"reflect"
-// 	"time"
+import (
+	"context"
+	"net/http"
+	"net/url"
+	"reflect"
+	"time"
 
-// 	"github.com/gorilla/websocket"
-// 	"github.com/pkg/errors"
-// 	"github.com/siyul-park/uniflow/pkg/node"
-// 	"github.com/siyul-park/uniflow/pkg/packet"
-// 	"github.com/siyul-park/uniflow/pkg/primitive"
-// 	"github.com/siyul-park/uniflow/pkg/process"
-// 	"github.com/siyul-park/uniflow/pkg/scheme"
-// 	"github.com/siyul-park/uniflow/plugin/internal/language"
-// )
+	"github.com/gorilla/websocket"
+	"github.com/pkg/errors"
+	"github.com/siyul-park/uniflow/pkg/node"
+	"github.com/siyul-park/uniflow/pkg/packet"
+	"github.com/siyul-park/uniflow/pkg/primitive"
+	"github.com/siyul-park/uniflow/pkg/process"
+	"github.com/siyul-park/uniflow/pkg/scheme"
+	"github.com/siyul-park/uniflow/plugin/internal/language"
+)
 
-// // WebSocketClientNode represents a node for establishing WebSocket client connections.
-// type WebSocketClientNode struct {
-// 	*WebSocketNode
-// 	dialer *websocket.Dialer
-// 	lang   string
-// 	url    func(any) (string, error)
-// }
+// WebSocketClientNode represents a node for establishing WebSocket client connections.
+type WebSocketClientNode struct {
+	*WebSocketNode
+	dialer *websocket.Dialer
+	lang   string
+	url    func(any) (string, error)
+}
 
-// // WebSocketClientNodeSpec holds the specifications for creating a WebSocketClientNode.
-// type WebSocketClientNodeSpec struct {
-// 	scheme.SpecMeta `map:",inline"`
-// 	Lang            string        `map:"lang,omitempty"`
-// 	URL             string        `map:"url,omitempty"`
-// 	Timeout         time.Duration `map:"timeout"`
-// }
+// WebSocketClientNodeSpec holds the specifications for creating a WebSocketClientNode.
+type WebSocketClientNodeSpec struct {
+	scheme.SpecMeta `map:",inline"`
+	Lang            string        `map:"lang,omitempty"`
+	URL             string        `map:"url,omitempty"`
+	Timeout         time.Duration `map:"timeout"`
+}
 
-// const KindWebSocketClient = "websocket/client"
+const KindWebSocketClient = "websocket/client"
 
-// var _ node.Node = (*WebSocketClientNode)(nil)
+var _ node.Node = (*WebSocketClientNode)(nil)
 
-// // NewWebSocketClientNode creates a new WebSocketClientNode.
-// func NewWebSocketClientNode() *WebSocketClientNode {
-// 	n := &WebSocketClientNode{
-// 		dialer: &websocket.Dialer{
-// 			Proxy:            http.ProxyFromEnvironment,
-// 			HandshakeTimeout: 45 * time.Second,
-// 		},
-// 	}
-// 	n.WebSocketNode = newWebSocketNode(n.connect)
+// NewWebSocketClientNode creates a new WebSocketClientNode.
+func NewWebSocketClientNode() *WebSocketClientNode {
+	n := &WebSocketClientNode{
+		dialer: &websocket.Dialer{
+			Proxy:            http.ProxyFromEnvironment,
+			HandshakeTimeout: 45 * time.Second,
+		},
+	}
+	n.WebSocketNode = newWebSocketNode(n.connect)
 
-// 	_ = n.SetURL("")
+	_ = n.SetURL("")
 
-// 	return n
-// }
+	return n
+}
 
-// // SetLanguage sets the language used for transformation.
-// func (n *WebSocketClientNode) SetLanguage(lang string) {
-// 	n.mu.Lock()
-// 	defer n.mu.Unlock()
+// SetLanguage sets the language used for transformation.
+func (n *WebSocketClientNode) SetLanguage(lang string) {
+	n.mu.Lock()
+	defer n.mu.Unlock()
 
-// 	n.lang = lang
-// }
+	n.lang = lang
+}
 
-// // SetURL sets the WebSocket URL.
-// func (n *WebSocketClientNode) SetURL(rawURL string) error {
-// 	n.mu.Lock()
-// 	defer n.mu.Unlock()
+// SetURL sets the WebSocket URL.
+func (n *WebSocketClientNode) SetURL(rawURL string) error {
+	n.mu.Lock()
+	defer n.mu.Unlock()
 
-// 	if rawURL == "" {
-// 		n.url = func(input any) (string, error) {
-// 			v := &url.URL{Scheme: "https"}
+	if rawURL == "" {
+		n.url = func(input any) (string, error) {
+			v := &url.URL{Scheme: "https"}
 
-// 			value := reflect.ValueOf(input)
-// 			if value.Kind() == reflect.Map {
-// 				if e := value.MapIndex(reflect.ValueOf("url")); e.IsValid() {
-// 					if e, ok := e.Interface().(string); ok {
-// 						var err error
-// 						if v, err = url.Parse(e); err != nil {
-// 							return "", err
-// 						}
-// 					}
-// 				}
+			value := reflect.ValueOf(input)
+			if value.Kind() == reflect.Map {
+				if e := value.MapIndex(reflect.ValueOf("url")); e.IsValid() {
+					if e, ok := e.Interface().(string); ok {
+						var err error
+						if v, err = url.Parse(e); err != nil {
+							return "", err
+						}
+					}
+				}
 
-// 				if e := value.MapIndex(reflect.ValueOf("scheme")); e.IsValid() {
-// 					if e, ok := e.Interface().(string); ok {
-// 						v.Scheme = e
-// 					}
-// 				}
-// 				if e := value.MapIndex(reflect.ValueOf("host")); e.IsValid() {
-// 					if e, ok := e.Interface().(string); ok {
-// 						v.Host = e
-// 					}
-// 				}
-// 				if e := value.MapIndex(reflect.ValueOf("path")); e.IsValid() {
-// 					if e, ok := e.Interface().(string); ok {
-// 						v.Path = e
-// 					}
-// 				}
-// 			}
+				if e := value.MapIndex(reflect.ValueOf("scheme")); e.IsValid() {
+					if e, ok := e.Interface().(string); ok {
+						v.Scheme = e
+					}
+				}
+				if e := value.MapIndex(reflect.ValueOf("host")); e.IsValid() {
+					if e, ok := e.Interface().(string); ok {
+						v.Host = e
+					}
+				}
+				if e := value.MapIndex(reflect.ValueOf("path")); e.IsValid() {
+					if e, ok := e.Interface().(string); ok {
+						v.Path = e
+					}
+				}
+			}
 
-// 			return v.String(), nil
-// 		}
-// 		return nil
-// 	}
+			return v.String(), nil
+		}
+		return nil
+	}
 
-// 	lang := n.lang
-// 	transform, err := language.CompileTransform(rawURL, &lang)
-// 	if err != nil {
-// 		return err
-// 	}
+	lang := n.lang
+	transform, err := language.CompileTransform(rawURL, &lang)
+	if err != nil {
+		return err
+	}
 
-// 	n.url = func(input any) (string, error) {
-// 		if output, err := transform(input); err != nil {
-// 			return "", err
-// 		} else if v, ok := output.(string); ok {
-// 			return v, nil
-// 		}
-// 		return "", errors.WithStack(packet.ErrInvalidPacket)
-// 	}
-// 	return nil
-// }
+	n.url = func(input any) (string, error) {
+		if output, err := transform(input); err != nil {
+			return "", err
+		} else if v, ok := output.(string); ok {
+			return v, nil
+		}
+		return "", errors.WithStack(packet.ErrInvalidPacket)
+	}
+	return nil
+}
 
-// // SetTimeout sets the handshake timeout for WebSocket connections.
-// func (n *WebSocketClientNode) SetTimeout(timeout time.Duration) {
-// 	n.mu.Lock()
-// 	defer n.mu.Unlock()
+// SetTimeout sets the handshake timeout for WebSocket connections.
+func (n *WebSocketClientNode) SetTimeout(timeout time.Duration) {
+	n.mu.Lock()
+	defer n.mu.Unlock()
 
-// 	n.dialer.HandshakeTimeout = timeout
-// }
+	n.dialer.HandshakeTimeout = timeout
+}
 
-// func (n *WebSocketClientNode) connect(proc *process.Process, inPck *packet.Packet) (*websocket.Conn, error) {
-// 	ctx := proc.Context()
+func (n *WebSocketClientNode) connect(proc *process.Process, inPck *packet.Packet) (*websocket.Conn, error) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-// 	inPayload := inPck.Payload()
-// 	input := primitive.Interface(inPayload)
+	inPayload := inPck.Payload()
+	input := primitive.Interface(inPayload)
 
-// 	rawURL, err := n.url(input)
-// 	if err != nil {
-// 		return nil, err
-// 	}
+	rawURL, err := n.url(input)
+	if err != nil {
+		return nil, err
+	}
 
-// 	conn, _, err := n.dialer.DialContext(ctx, rawURL, nil)
-// 	return conn, err
-// }
+	conn, _, err := n.dialer.DialContext(ctx, rawURL, nil)
+	return conn, err
+}
 
-// // NewWebSocketClientNodeCodec creates a new codec for WebSocketClientNodeSpec.
-// func NewWebSocketClientNodeCodec() scheme.Codec {
-// 	return scheme.CodecWithType(func(spec *WebSocketClientNodeSpec) (node.Node, error) {
-// 		n := NewWebSocketClientNode()
-// 		n.SetLanguage(spec.Lang)
-// 		n.SetURL(spec.URL)
-// 		n.SetTimeout(spec.Timeout)
-// 		return n, nil
-// 	})
-// }
+// NewWebSocketClientNodeCodec creates a new codec for WebSocketClientNodeSpec.
+func NewWebSocketClientNodeCodec() scheme.Codec {
+	return scheme.CodecWithType(func(spec *WebSocketClientNodeSpec) (node.Node, error) {
+		n := NewWebSocketClientNode()
+		n.SetLanguage(spec.Lang)
+		n.SetURL(spec.URL)
+		n.SetTimeout(spec.Timeout)
+		return n, nil
+	})
+}
