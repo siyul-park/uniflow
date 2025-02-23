1ae0a141880d88b2cc76b7deed903f144bd5ec5e
MagicWorld <craftedmovie30@gmail.com>
2025-02-24 00:26:18 +0900

    feat: re-implemented Run function

--- ext/pkg/testing/test.go
+++ ext/pkg/testing/test.go
@@ -4,7 +4,6 @@ import (
 	"github.com/siyul-park/uniflow/pkg/node"
 	"github.com/siyul-park/uniflow/pkg/packet"
 	"github.com/siyul-park/uniflow/pkg/port"
-	"github.com/siyul-park/uniflow/pkg/process"
 	"github.com/siyul-park/uniflow/pkg/testing"
 	"github.com/siyul-park/uniflow/pkg/types"
 )
@@ -24,38 +23,49 @@ func NewTestNode() *TestNode {
 }
 
 func (n *TestNode) Run(t *testing.Tester) {
-	// 0. create process to use writer0 and writer1
-	proc := process.New()
-	defer proc.Exit(nil)
-
 	// 1. open writer0, writer1
-	writer0 := n.outPorts[0].Open(proc)
-	writer1 := n.outPorts[1].Open(proc)
+	process := t.Process()
+	writer0 := n.outPorts[0].Open(process)
+	writer1 := n.outPorts[1].Open(process)
 
 	// 2. write outPck0 to writer0, payload is nil
 	outPck0 := packet.New(nil)
 	writer0.Write(outPck0)
+	// if writer0.Write(outPck0) == 0 {
+	// 	t.Exit(nil)
+	// 	return
+	// }
 
 	// 3. receive backPck0 in writer0
 	backPck0 := <-writer0.Receive()
+	// if backPck0 == packet.None {
+	// 	t.Exit(nil)
+	// 	return
+	// }
 
 	// 4. create outPck1, payload is [backPck0.Payload(), -1]
-	outPck1 := packet.New(types.NewSlice(backPck0.Payload(), types.NewInt(-1)))
+	outPck1 := packet.New(types.NewSlice(backPck0.Payload(), types.NewInt32(-1)))
 
 	// 5. write outPck1 to writer1
-	count := writer1.Write(outPck1)
+	writeCount := writer1.Write(outPck1)
 
-	// 6. check write outPck1 is success
-	if count == 0 {
+	// 6. check write ouPck1 is success, check writer1.Write output
+	if writeCount == 0 {
 		// 7. if write is fail, check backPck0 payload is error, and exit tester as backPck0 payload
 		if err, ok := backPck0.Payload().(types.Error); ok {
 			t.Exit(err.Unwrap())
 			return
 		}
+		t.Exit(nil)
+		return
 	}
 
 	// 8. if write is success, receive backPck1 in writer1
 	backPck1 := <-writer1.Receive()
+	// if backPck1 == packet.None {
+	// 	t.Exit(nil)
+	// 	return
+	// }
 
 	// 9. if backPck1 payload is error, exit tester as backPck1 error payload
 	if err, ok := backPck1.Payload().(types.Error); ok {
