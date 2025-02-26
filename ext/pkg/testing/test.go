63ac5faf645ffcd6ebcdf3f9667cedf1c6475b87
MagicWorld <craftedmovie30@gmail.com>
2025-02-26 10:31:16 +0900

    refactor: Write to Send

--- ext/pkg/testing/test.go
+++ ext/pkg/testing/test.go
@@ -23,57 +23,36 @@ func NewTestNode() *TestNode {
 }
 
 func (n *TestNode) Run(t *testing.Tester) {
-	// 1. open writer0, writer1
 	proc := t.Process()
 	writer0 := n.outPorts[0].Open(proc)
 	writer1 := n.outPorts[1].Open(proc)
 
-	// 2. write outPck0 to writer0, payload is nil
 	outPck0 := packet.New(nil)
-	writer0.Write(outPck0)
-	if writer0.Write(outPck0) == 0 {
-		t.Exit(nil)
-		return
-	}
+	backPck0 := packet.Send(writer0, outPck0)
 
-	// 3. receive backPck0 in writer0
-	backPck0 := <-writer0.Receive()
 	if backPck0 == packet.None {
 		t.Exit(nil)
 		return
 	}
 
-	// 4. create outPck1, payload is [backPck0.Payload(), -1]
-	outPck1 := packet.New(types.NewSlice(backPck0.Payload(), types.NewInt32(-1)))
-
-	// 5. write outPck1 to writer1
-	writeCount := writer1.Write(outPck1)
-
-	// 6. check write ouPck1 is success, check writer1.Write output
-	if writeCount == 0 {
-		// 7. if write is fail, check backPck0 payload is error, and exit tester as backPck0 payload
-		if err, ok := backPck0.Payload().(types.Error); ok {
-			t.Exit(err.Unwrap())
-			return
-		}
-		t.Exit(nil)
+	if err, ok := backPck0.Payload().(types.Error); ok {
+		t.Exit(err.Unwrap())
 		return
 	}
 
-	// 8. if write is success, receive backPck1 in writer1
-	backPck1 := <-writer1.Receive()
+	outPck1 := packet.New(types.NewSlice(backPck0.Payload(), types.NewInt32(-1)))
+	backPck1 := packet.Send(writer1, outPck1)
+
 	if backPck1 == packet.None {
 		t.Exit(nil)
 		return
 	}
 
-	// 9. if backPck1 payload is error, exit tester as backPck1 error payload
 	if err, ok := backPck1.Payload().(types.Error); ok {
 		t.Exit(err.Unwrap())
 		return
 	}
 
-	// 10 if not, exit tester no error
 	t.Exit(nil)
 }
 
