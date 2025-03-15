6c75de67e08fcf205dd63bde6edb1a822ad1c384
Ara Park <siyual.bak@gmail.com>
2025-03-16 01:11:08 +0900

    feat: implement test node (#242)

---
+++ ext/pkg/testing/test.go
@@ -0,0 +1,99 @@
+package testing
+
+import (
+	"github.com/siyul-park/uniflow/pkg/node"
+	"github.com/siyul-park/uniflow/pkg/packet"
+	"github.com/siyul-park/uniflow/pkg/port"
+	"github.com/siyul-park/uniflow/pkg/scheme"
+	"github.com/siyul-park/uniflow/pkg/spec"
+	"github.com/siyul-park/uniflow/pkg/testing"
+	"github.com/siyul-park/uniflow/pkg/types"
+)
+
+// TestNodeSpec defines the specifications for creating a TestNode.
+type TestNodeSpec struct {
+	spec.Meta `json:",inline"`
+}
+
+// TestNode is a test node implementing node.Node and testing.Suite interfaces.
+type TestNode struct {
+	outPorts [2]*port.OutPort
+}
+
+const KindTest = "test"
+
+var _ node.Node = (*TestNode)(nil)
+var _ testing.Suite = (*TestNode)(nil)
+
+// NewTestNodeCodec creates and returns a codec for decoding TestNodeSpec.
+func NewTestNodeCodec() scheme.Codec {
+	return scheme.CodecWithType(func(_ *TestNodeSpec) (node.Node, error) {
+		return NewTestNode(), nil
+	})
+}
+
+// NewTestNode creates and returns a new instance of TestNode.
+func NewTestNode() *TestNode {
+	return &TestNode{outPorts: [2]*port.OutPort{port.NewOut(), port.NewOut()}}
+}
+
+// Run executes the test logic, sending packets through output ports and handling errors.
+func (n *TestNode) Run(t *testing.Tester) {
+	proc := t.Process()
+
+	writer0 := n.outPorts[0].Open(proc)
+	writer1 := n.outPorts[1].Open(proc)
+
+	outPck0 := packet.New(nil)
+	backPck0 := packet.Send(writer0, outPck0)
+	if backPck0 == packet.None {
+		t.Exit(nil)
+		return
+	}
+	if err, ok := backPck0.Payload().(types.Error); ok {
+		t.Exit(err.Unwrap())
+		return
+	}
+
+	outPck1 := packet.New(types.NewSlice(backPck0.Payload(), types.NewInt(-1)))
+	backPck1 := packet.Send(writer1, outPck1)
+	if backPck1 == packet.None {
+		t.Exit(nil)
+		return
+	}
+	if err, ok := backPck1.Payload().(types.Error); ok {
+		t.Exit(err.Unwrap())
+		return
+	}
+
+	t.Exit(nil)
+}
+
+// In returns nil as this node does not use an input port.
+func (n *TestNode) In(_ string) *port.InPort {
+	return nil
+}
+
+// Out returns the output port specified by the name.
+func (n *TestNode) Out(name string) *port.OutPort {
+	switch name {
+	case node.PortOut:
+		return n.outPorts[0]
+	default:
+		if node.NameOfPort(name) == node.PortOut {
+			index, ok := node.IndexOfPort(name)
+			if ok && index < len(n.outPorts) {
+				return n.outPorts[index]
+			}
+		}
+	}
+	return nil
+}
+
+// Close closes all output ports of the TestNode.
+func (n *TestNode) Close() error {
+	for _, outPort := range n.outPorts {
+		outPort.Close()
+	}
+	return nil
+}
---
+++ ext/pkg/testing/test_test.go
@@ -0,0 +1,186 @@
+package testing
+
+import (
+	"context"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/go-faker/faker/v4"
+	"github.com/pkg/errors"
+	"github.com/siyul-park/uniflow/pkg/node"
+	"github.com/siyul-park/uniflow/pkg/packet"
+	"github.com/siyul-park/uniflow/pkg/process"
+	testing2 "github.com/siyul-park/uniflow/pkg/testing"
+	"github.com/siyul-park/uniflow/pkg/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewTestNode(t *testing.T) {
+	n := NewTestNode()
+	require.NotNil(t, n)
+	require.NoError(t, n.Close())
+}
+
+func TestTestNode_Port(t *testing.T) {
+	n := NewTestNode()
+	defer n.Close()
+
+	require.NotNil(t, n.Out(node.PortOut))
+	require.NotNil(t, n.Out(node.PortWithIndex(node.PortOut, 0)))
+	require.NotNil(t, n.Out(node.PortWithIndex(node.PortOut, 1)))
+}
+
+func TestPipeNode_SendAndReceive(t *testing.T) {
+	t.Run("SingleOutput", func(t *testing.T) {
+		ctx, cancel := context.WithTimeout(context.TODO(), 60*time.Second)
+		defer cancel()
+
+		n1 := NewTestNode()
+		defer n1.Close()
+
+		var count atomic.Int32
+		n2 := node.NewOneToOneNode(func(_ *process.Process, inPck *packet.Packet) (*packet.Packet, *packet.Packet) {
+			count.Add(1)
+			return inPck, nil
+		})
+		defer n2.Close()
+
+		n1.Out(node.PortWithIndex(node.PortOut, 0)).Link(n2.In(node.PortIn))
+
+		tester := testing2.NewTester("")
+		defer tester.Exit(nil)
+
+		go n1.Run(tester)
+
+		select {
+		case <-tester.Done():
+			require.Equal(t, int32(1), count.Load())
+			require.ErrorIs(t, tester.Err(), context.Canceled)
+		case <-ctx.Done():
+			require.Fail(t, ctx.Err().Error())
+		}
+	})
+
+	t.Run("SingleError", func(t *testing.T) {
+		ctx, cancel := context.WithTimeout(context.TODO(), 60*time.Second)
+		defer cancel()
+
+		n1 := NewTestNode()
+		defer n1.Close()
+
+		var count atomic.Int32
+		n2 := node.NewOneToOneNode(func(_ *process.Process, inPck *packet.Packet) (*packet.Packet, *packet.Packet) {
+			count.Add(1)
+			return nil, packet.New(types.NewError(errors.New(faker.Sentence())))
+		})
+		defer n2.Close()
+
+		n1.Out(node.PortWithIndex(node.PortOut, 0)).Link(n2.In(node.PortIn))
+
+		tester := testing2.NewTester("")
+		defer tester.Exit(nil)
+
+		go n1.Run(tester)
+
+		select {
+		case <-tester.Done():
+			require.Equal(t, int32(1), count.Load())
+			require.Error(t, tester.Err())
+			require.NotErrorIs(t, tester.Err(), context.Canceled)
+		case <-ctx.Done():
+			require.Fail(t, ctx.Err().Error())
+		}
+	})
+
+	t.Run("MultiOutput", func(t *testing.T) {
+		ctx, cancel := context.WithTimeout(context.TODO(), 60*time.Second)
+		defer cancel()
+
+		n1 := NewTestNode()
+		defer n1.Close()
+
+		var count atomic.Int32
+
+		n2 := node.NewOneToOneNode(func(_ *process.Process, inPck *packet.Packet) (*packet.Packet, *packet.Packet) {
+			count.Add(1)
+			return packet.New(types.NewString(faker.Sentence())), nil
+		})
+		defer n2.Close()
+
+		n3 := node.NewOneToOneNode(func(_ *process.Process, inPck *packet.Packet) (*packet.Packet, *packet.Packet) {
+			count.Add(1)
+
+			inPayload, ok := inPck.Payload().(types.Slice)
+			require.True(t, ok)
+			require.Equal(t, 2, inPayload.Len())
+			require.Equal(t, types.KindString, inPayload.Get(0).Kind())
+			require.Equal(t, types.KindInt, inPayload.Get(1).Kind())
+
+			return inPck, nil
+		})
+		defer n3.Close()
+
+		n1.Out(node.PortWithIndex(node.PortOut, 0)).Link(n2.In(node.PortIn))
+		n1.Out(node.PortWithIndex(node.PortOut, 1)).Link(n3.In(node.PortIn))
+
+		tester := testing2.NewTester("")
+		defer tester.Exit(nil)
+
+		go n1.Run(tester)
+
+		select {
+		case <-tester.Done():
+			require.Equal(t, int32(2), count.Load())
+			require.ErrorIs(t, tester.Err(), context.Canceled)
+		case <-ctx.Done():
+			require.Fail(t, ctx.Err().Error())
+		}
+	})
+
+	t.Run("MultiError", func(t *testing.T) {
+		ctx, cancel := context.WithTimeout(context.TODO(), 60*time.Second)
+		defer cancel()
+
+		n1 := NewTestNode()
+		defer n1.Close()
+
+		var count atomic.Int32
+
+		n2 := node.NewOneToOneNode(func(_ *process.Process, inPck *packet.Packet) (*packet.Packet, *packet.Packet) {
+			count.Add(1)
+			return packet.New(types.NewString(faker.Sentence())), nil
+		})
+		defer n2.Close()
+
+		n3 := node.NewOneToOneNode(func(_ *process.Process, inPck *packet.Packet) (*packet.Packet, *packet.Packet) {
+			count.Add(1)
+
+			inPayload, ok := inPck.Payload().(types.Slice)
+			require.True(t, ok)
+			require.Equal(t, 2, inPayload.Len())
+			require.Equal(t, types.KindString, inPayload.Get(0).Kind())
+			require.Equal(t, types.KindInt, inPayload.Get(1).Kind())
+
+			return nil, packet.New(types.NewError(errors.New(faker.Sentence())))
+		})
+		defer n3.Close()
+
+		n1.Out(node.PortWithIndex(node.PortOut, 0)).Link(n2.In(node.PortIn))
+		n1.Out(node.PortWithIndex(node.PortOut, 1)).Link(n3.In(node.PortIn))
+
+		tester := testing2.NewTester("")
+		defer tester.Exit(nil)
+
+		go n1.Run(tester)
+
+		select {
+		case <-tester.Done():
+			require.Equal(t, int32(2), count.Load())
+			require.Error(t, tester.Err())
+			require.NotErrorIs(t, tester.Err(), context.Canceled)
+		case <-ctx.Done():
+			require.Fail(t, ctx.Err().Error())
+		}
+	})
+}
